56776446ea01ff0c718540ef8ef1f2eae67466a1
cis101 <cis101@secs.iub.edu.bd>
2019-07-08 09:32:56 +0600

    api endpoints

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var http = require('http');
+var express = require('express');
+var app = express();
+var server= https.Server(app)
 
-var fs = require('fs');
-
-var server = http.createServer(function(req, res){
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  fs.readFile('index.html', function(err,data){
-    if (err){
-       return console.log("File read error")
-    }  
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+app.get('/second',function(req,res){
+    res.sendFile(__dirname+'/second.html')
+})
+// var server = http.createServer(function(req, res){
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   fs.readFile('index.html', function(err,data){
+//     if (err){
+//        return console.log("File read error")
+//     }  
     
-    res.end(data);
-  })
-});
+//     res.end(data);
+//   })
+// });
 server.listen(3000, 'localhost', function(){
   console.log('Server running');
 });
